0629b48b1a5d1f3b45de7db28bc96d786c843ddd
vozniack <t.vozniackk@gmail.com>
2025-06-07 17:28:32 +0200

    Added difficulty select dialog

--- soodoku-web-app/src/app/modules/game/game-info/game-info.component.ts
+++ soodoku-web-app/src/app/modules/game/game-info/game-info.component.ts
@@ -4,8 +4,10 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Store } from '@ngrx/store';
 import { tap } from 'rxjs/operators';
 import { Breakpoint } from '../../../core/breakpoint/breakpoint.interface';
+import { ModalService } from '../../../core/modal/modal.service';
 import { AvatarComponent } from '../../../shared/components/avatar/avatar.component';
 import { ButtonComponent } from '../../../shared/components/button/button.component';
+import { DifficultyDialogComponent } from '../../../shared/dialogs/difficulty-dialog/difficulty-dialog.component';
 import { SELECT_BREAKPOINT } from '../../../store/app/app.selectors';
 
 @Component({
@@ -19,10 +21,14 @@ export class GameInfoComponent {
 
   breakpoint!: Breakpoint;
 
-  constructor(private store: Store) {
+  constructor(private store: Store, private modalService: ModalService) {
     this.store.select(SELECT_BREAKPOINT).pipe(
       takeUntilDestroyed(),
       tap((breakpoint: Breakpoint) => this.breakpoint = breakpoint)
     ).subscribe();
+  }
+
+  newGame(): void {
+    this.modalService.open(DifficultyDialogComponent);
   }
 }
---
+++ soodoku-web-app/src/app/shared/dialogs/difficulty-dialog/difficulty-dialog.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'soo-difficulty-dialog',
+  standalone: true,
+  templateUrl: './difficulty-dialog.component.html',
+  styleUrl: './difficulty-dialog.component.scss',
+})
+export class DifficultyDialogComponent {
+
+}
