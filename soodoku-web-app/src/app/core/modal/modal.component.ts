529e4645dd0b1128c333e376465d3a88de086f4d
vozniack <t.vozniackk@gmail.com>
2025-06-07 17:08:10 +0200

    Added dynamic modal window

---
+++ soodoku-web-app/src/app/core/modal/modal.component.ts
@@ -0,0 +1,61 @@
+import { Component, EventEmitter, Output, Type, ViewChild, ViewContainerRef, } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { tap } from 'rxjs/operators';
+import { ButtonComponent } from '../../shared/components/button/button.component';
+import { SELECT_BREAKPOINT } from '../../store/app/app.selectors';
+import { Breakpoint } from '../breakpoint/breakpoint.interface';
+
+@Component({
+  selector: 'soo-modal',
+  standalone: true,
+  templateUrl: './modal.component.html',
+  styleUrls: ['./modal.component.scss'],
+  imports: [ButtonComponent]
+})
+export class ModalComponent {
+
+  @Output() closed = new EventEmitter<void>();
+
+  @ViewChild('container', {read: ViewContainerRef, static: true})
+  container!: ViewContainerRef;
+
+  breakpoint!: Breakpoint;
+
+  hidden = false;
+
+  constructor(private store: Store) {
+    this.store.select(SELECT_BREAKPOINT).pipe(
+      tap((breakpoint: Breakpoint) => this.breakpoint = breakpoint)
+    ).subscribe();
+  }
+
+  loadChild<T>(component: Type<T>, inputs?: Partial<T>) {
+    this.container.clear();
+
+    const compRef = this.container.createComponent(component);
+
+    if (inputs) {
+      Object.assign(compRef.instance as object, inputs);
+    }
+  }
+
+  onBackdropClick(event: MouseEvent) {
+    if (!this.isMobile) {
+      return;
+    }
+
+    if ((event.target as HTMLElement).classList.contains('backdrop')) {
+      this.close();
+    }
+  }
+
+  close() {
+    this.hidden = true;
+
+    setTimeout(() => this.closed.emit(), 128);
+  }
+
+  get isMobile(): boolean {
+    return this.breakpoint == 'xs';
+  }
+}
