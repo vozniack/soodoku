dac0b16b10fa9ba05bd26dc9512974978fb559fc
vozniack <t.vozniackk@gmail.com>
2025-06-19 19:50:44 +0200

    Improved endgame icons mapping

---
+++ soodoku-web-app/src/app/modules/game/game.const.ts
@@ -0,0 +1,2 @@
+export const VICTORY_ICON = 'sentiment_very_satisfied'
+export const DEFEAT_ICON = 'sentiment_dissatisfied'
--- soodoku-web-app/src/app/shared/components/game-tile/game-tile.component.ts
+++ soodoku-web-app/src/app/shared/components/game-tile/game-tile.component.ts
@@ -2,6 +2,7 @@ import { DatePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { View } from '../../../core/view/view.const';
+import { DEFEAT_ICON, VICTORY_ICON } from '../../../modules/game/game.const';
 import { Game } from '../../../modules/game/game.interface';
 import { ACTION_SET_VIEW } from '../../../store/app/app.actions';
 import { ACTION_GAME_SET } from '../../../store/app/game/game.actions';
@@ -37,10 +38,10 @@ export class GameTileComponent {
   getIcon(): string {
     switch (this.getState()) {
       case 'won':
-        return 'trophy';
+        return VICTORY_ICON;
 
       case 'lost':
-        return 'skull';
+        return DEFEAT_ICON;
 
       default:
         return 'play_arrow';
