406094c89c67bb1b25d807d68ec3f9bbce647fdb
vozniack <t.vozniackk@gmail.com>
2025-10-02 21:21:46 +0200

    Renamed variables

--- soodoku-web-app/src/app/core/sse/sse.friend.handler.ts
+++ soodoku-web-app/src/app/core/sse/sse.friend.handler.ts
@@ -13,10 +13,10 @@ export function registerFriendHandlers(
 ): void {
   sseService.on<{ invitationId: string; senderUsername: string }>(
     'FRIEND_INVITATION_RECEIVED',
-    (p) => {
-      store.dispatch(ACTION_FRIEND_INVITATION_RECEIVED({invitationId: p.invitationId}));
+    (payload) => {
+      store.dispatch(ACTION_FRIEND_INVITATION_RECEIVED({invitationId: payload.invitationId}));
       store.dispatch(ACTION_SHOW_SNACKBAR({
-        message: `${p.senderUsername} has invited you to friends!`,
+        message: `${payload.senderUsername} has invited you to friends!`,
         icon: 'notifications'
       }));
     }
@@ -24,10 +24,10 @@ export function registerFriendHandlers(
 
   sseService.on<{ invitationId: string; receiverUsername: string }>(
     'FRIEND_INVITATION_ACCEPTED',
-    (p) => {
-      store.dispatch(ACTION_FRIEND_INVITATION_ACCEPTED({invitationId: p.invitationId}));
+    (payload) => {
+      store.dispatch(ACTION_FRIEND_INVITATION_ACCEPTED({invitationId: payload.invitationId}));
       store.dispatch(ACTION_SHOW_SNACKBAR({
-        message: `${p.receiverUsername} has accepted your friend invitation!`,
+        message: `${payload.receiverUsername} has accepted your friend invitation!`,
         icon: 'notifications'
       }));
     }
@@ -35,10 +35,10 @@ export function registerFriendHandlers(
 
   sseService.on<{ invitationId: string; receiverUsername: string }>(
     'FRIEND_INVITATION_REJECTED',
-    (p) => {
-      store.dispatch(ACTION_FRIEND_INVITATION_REJECTED({invitationId: p.invitationId}));
+    (payload) => {
+      store.dispatch(ACTION_FRIEND_INVITATION_REJECTED({invitationId: payload.invitationId}));
       store.dispatch(ACTION_SHOW_SNACKBAR({
-        message: `${p.receiverUsername} has rejected your friend invitation!`,
+        message: `${payload.receiverUsername} has rejected your friend invitation!`,
         icon: 'notifications'
       }));
     }
@@ -46,10 +46,10 @@ export function registerFriendHandlers(
 
   sseService.on<{ invitationId: string; senderUsername: string }>(
     'FRIEND_INVITATION_REMOVED',
-    (p) => {
-      store.dispatch(ACTION_FRIEND_INVITATION_REMOVED({invitationId: p.invitationId}));
+    (payload) => {
+      store.dispatch(ACTION_FRIEND_INVITATION_REMOVED({invitationId: payload.invitationId}));
       store.dispatch(ACTION_SHOW_SNACKBAR({
-        message: `${p.senderUsername} has withdrawn friend invitation!`,
+        message: `${payload.senderUsername} has withdrawn friend invitation!`,
         icon: 'notifications'
       }));
     }
@@ -57,10 +57,10 @@ export function registerFriendHandlers(
 
   sseService.on<{ friendUsername: string }>(
     'FRIEND_REMOVED',
-    (p) => {
-      store.dispatch(ACTION_FRIEND_REMOVED({friendUsername: p.friendUsername}));
+    (payload) => {
+      store.dispatch(ACTION_FRIEND_REMOVED({friendUsername: payload.friendUsername}));
       store.dispatch(ACTION_SHOW_SNACKBAR({
-        message: `${p.friendUsername} has removed a friendship!`,
+        message: `${payload.friendUsername} has removed a friendship!`,
         icon: 'notifications'
       }));
     }
